4ab0d7dc883dd1d189f0385498f5132c2ab077bf
nigoi <87484366+nigoi@users.noreply.github.com>
2025-02-17 11:48:35 +0000

    scoreboard fix
    now does full tennis scoring

--- scripts/label_master.gd
+++ scripts/label_master.gd
@@ -1,37 +1,37 @@
 extends Node
 var player1 = null
 var player2 = null
-var score = {"player 1": 40, "player 2": 0}
+var score = {"player 1": 0, "player 2": 30}
 
 func tennisify(player1_score, player2_score):
 	if typeof(score [player1_score]) == TYPE_INT:
 		while score [player1_score] < 40:
 			if score [player1_score] < 30:
 				score [player1_score] += 15
+				return
 			else:
+				score [player1_score] < 40
 				score [player1_score] += 10
+				# handle deuce		
+				if score [player1_score] == 40 and score [player2_score] == 40:
+					score [player1_score] = "Duece"
+					score [player2_score] = "Duece"
+				# handle score win
+				elif score [player1_score] == 40 and score [player2_score] < 40:
+					score [player1_score] = "YOU WIN!"
+					score [player2_score] = ""
+					get_tree().reload_current_scene()
 			return
-	# handle win
-	if score [player1_score] == 40 and score [player2_score] < 40:
-				score [player1_score] = "YOU WIN!"
-				get_tree().reload_current_scene()
-	# handle duece
-	elif score [player1_score] and score [player2_score] == 40:
-		score [player1_score] = "Deuce"
-		score [player2_score] = "Deuce"
-		
-	
+	#handle advantage + advantage type win
+	if typeof(score [player1_score]) == TYPE_STRING:
+		if score [player1_score] == "Duece":
+			score [player1_score] = "Advantage"
+			score [player1_score] = ""
+		if score [player1_score] == "Advantage":
+			score [player1_score] = "YOU WIN!"
+			score [player2_score] = ""
+			get_tree().reload_current_scene()
 
-
-	
-	
-	# handle advantage
-	#elif score [player1_score] and score [player2_score] == "Deuce":
-		#score [player1_score] = "Advantage"
-		#score [player2_score] = 40
-	# handle game point 
-	
-	
 
 func _on_player_wall_2_body_entered(body: Node2D) -> void:
 	tennisify("player 1", "player 2")
@@ -48,7 +48,7 @@ func _on_player_wall_body_entered(body: Node2D) -> void:
 func _ready() -> void:
 	player1 = get_node("player1_label")
 	player2 = get_node("player2_label")
-	player1.position = Vector2(get_viewport().size.x / 2 - 50, get_viewport().size.y * 0.10)
-	player2.position = Vector2(get_viewport().size.x / 2 + 50, get_viewport().size.y * 0.10)
+	player1.position = Vector2(get_viewport().size.x / 2 - 300, get_viewport().size.y * 0.10)
+	player2.position = Vector2(get_viewport().size.x / 2 + 300, get_viewport().size.y * 0.10)
 	player1.text = str(score["player 1"])
 	player2.text = str(score["player 2"])
