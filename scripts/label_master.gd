26ca474775802d250fb5a50ebf4b0c456639453a
nigoi <87484366+nigoi@users.noreply.github.com>
2025-02-17 10:04:17 +0000

    more work on scoreboard

--- scripts/label_master.gd
+++ scripts/label_master.gd
@@ -1,35 +1,49 @@
 extends Node
 var player1 = null
 var player2 = null
-var score = {"player 1": 0, "player 2": 0}
+var score = {"player 1": 40, "player 2": 0}
 
-func tennisify(key, key2):
-	while score.has(key) and score.has(key2) and str(score[key]) != "Game Point" and str(score[key2]) != "Game Point":
-		if score[key] < 30:
-			score[key] += 15
-		elif score[key] == 40 and score[key2] == 40:
-			score[key] = "Deuce"
-			score[key2] = "Deuce"
-			break # stops loop
-		elif score[key] == 40 and score[key2] != "40":
-			score[key] = "Game Point"
-			break # game won
+func tennisify(player1_score, player2_score):
+	if typeof(score [player1_score]) == TYPE_INT:
+		while score [player1_score] < 40:
+			if score [player1_score] < 30:
+				score [player1_score] += 15
+			else:
+				score [player1_score] += 10
+			return
+	# handle win
+	if score [player1_score] == 40 and score [player2_score] < 40:
+				score [player1_score] = "YOU WIN!"
+				get_tree().reload_current_scene()
+	# handle duece
+	elif score [player1_score] and score [player2_score] == 40:
+		score [player1_score] = "Deuce"
+		score [player2_score] = "Deuce"
+		
+	
 
-	#handling duece and advantage
-	if score[key] == "Deuce" and score[key2] == "Deuce":
-		score[key] = "Advantage"
-		score[key2] = "40"
-	elif score[key] == "Advantage":
-		score[key] = "Game Point" 
-		get_tree().reload_current_scene() # Player wins after Advantage
+
+	
+	
+	# handle advantage
+	#elif score [player1_score] and score [player2_score] == "Deuce":
+		#score [player1_score] = "Advantage"
+		#score [player2_score] = 40
+	# handle game point 
+	
+	
 
 func _on_player_wall_2_body_entered(body: Node2D) -> void:
 	tennisify("player 1", "player 2")
 	player1.text = str(score ["player 1"])
+	player2.text = str(score ["player 2"])
+	print(score)
 
 func _on_player_wall_body_entered(body: Node2D) -> void:
 	tennisify("player 2", "player 1")
-	player1.text = str(score ["player 2"])
+	player1.text = str(score ["player 1"])
+	player2.text = str(score ["player 2"])
+	print(score)
 
 func _ready() -> void:
 	player1 = get_node("player1_label")
